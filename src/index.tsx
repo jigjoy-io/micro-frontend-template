dd836d6f11aa92fdf84b061c2b991da4fc3bf8e0
Mijura <miodragvilotijevic@gmail.com>
2024-12-17 17:38:49 +0100

    deleted tenstack and redux from template

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
-import { createRouter, RouterProvider } from "@tanstack/react-router"
-import { NotFoundRoute } from "@tanstack/react-router"
-import { Route as rootRoute } from "./routes/__root"
-import { routeTree } from "./routeTree.gen"
-import { Provider } from "react-redux"
-import { store } from "./util/store"
-
-const notFoundRoute = new NotFoundRoute({
-	getParentRoute: () => rootRoute,
-	component: () => "404 Not Found",
-})
 
 const root = document.createElement("div")
 document.body.appendChild(root)
 const rootDiv = ReactDOM.createRoot(root)
 
-const router = createRouter({
-	routeTree,
-	notFoundRoute,
-})
-
-declare module "@tanstack/react-router" {
-	interface Register {
-		router: typeof router
-	}
-}
-
 rootDiv.render(
 	<React.StrictMode>
-		<Provider store={store}>
-			<RouterProvider router={router} />
-		</Provider>
-	</React.StrictMode>,
+		<h1>Hello, world!</h1>
+	</React.StrictMode>
 )
--- src/routes/index.lazy.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import { createLazyFileRoute } from "@tanstack/react-router"
-import CounterComponent from "../components/example-counter"
-
-export const Route: any = createLazyFileRoute("/" as never)({
-	component: () => <CounterComponent />,
-})
